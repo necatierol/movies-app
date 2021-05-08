95da585a0da7b440dacdacc4a2f68a07003affad
Necati Erol <necati.erol@wext.com>
2021-05-08 17:47:17 +0300

    content model changes

--- app/models/content.rb
+++ app/models/content.rb
@@ -1,2 +1,22 @@
 class Content < ApplicationRecord
+  # active storage
+  has_one_attached :logo
+  has_one_attached :cover
+  attr_accessor :remove_logo
+  attr_accessor :remove_cover
+  after_save { logo.purge if remove_logo == '1' }
+  after_save { cover.purge if remove_cover == '1' }
+
+  # enums
+  enum item_type: {
+    LIVE_TV: 'LIVE_TV',
+    LIVE_RADIO: 'LIVE_RADIO',
+    FILM: 'FILM',
+    TV_SERIES: 'TV_SERIES'
+  }
+
+  enum language: { TR: 'TR', EN: 'EN' }
+
+  # validations
+  validates :item_type, :language, presence: true
 end
